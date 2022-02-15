db4bb8bf4d9d48522cf628bd95d95fa23670e707
changbin93 <626498551@qq.com>
2022-02-15 15:30:43 +0800

    Initial commit

---
+++ src/main/java/com/changbin93/nicebbs/NicebbsApplication.java
@@ -0,0 +1,13 @@
+package com.changbin93.nicebbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicebbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NicebbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/changbin93/nicebbs/NicebbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changbin93.nicebbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicebbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
